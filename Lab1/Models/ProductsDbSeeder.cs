30fa383308abdc0731420f86356fdebe57e6d3cc
aly137 <gongolea@gmail.com>
2019-06-01 11:43:31 +0300

    1st try

---
+++ Lab1/Models/ProductsDbSeeder.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab1.Models
+{
+    public class ProductsDbSeeder
+    {
+
+        public static void Initialize(ProductsDbContext context)
+        {
+            context.Database.EnsureCreated();
+
+            // Look for any products.
+            if (context.Products.Any())
+            {
+                return;   // DB has been seeded
+            }
+
+            context.Products.AddRange(
+                new Product
+                {
+                    Name = "Milka",
+                    Description = "Milk",
+                    Category = "Chocolate",
+                    Price = 15
+                },
+                new Product
+                {
+                    Name = "Kinder",
+                    Description = "Bueno",
+                    Category = "Chocolate",
+                    Price = 12
+                }
+            );
+            context.SaveChanges();
+        }
+    }
+}
